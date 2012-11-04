e5b6f396614a5779f3bd777552ffec89e85e5bbd
Mat Kent <matkent@gmail.com>
2012-11-04 00:02:45 -0600

    final checkin

--- Project-5/more1.c
+++ Project-5/more1.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
+#include <signal.h>
 
 #define ON 1
 #define OFF 0
 #define PAGELEN 23
 #define LINELEN 512
 
-void DoMore(FILE*);
-int SeeMore();
+void DoMore(FILE*, int);
+int SeeMore(FILE*);
 int SetTcFlag(int, int, FILE*);
+void HandleSignals(int);
+void PrintStringReverseVideo(char*);
+int GetFileLineCount(char*);
+int CalcPercent(int, int);
 
 int main(int argc, char *argv[])
 {
+  signal(SIGINT, HandleSignals);
+
   int fd_tty;
   FILE *fp;
 
   if (argc == 1)
   {
-    DoMore(stdin);
+    DoMore(stdin, 0);
   }
   else
   {
-    fp = fopen(*++argv, "r");
+    char *fileName = *++argv;
+    int lnCount = GetFileLineCount(fileName);
+    fp = fopen(fileName, "r");
     if (fp != NULL)
     {
-      DoMore(fp);
+      DoMore(fp, lnCount);
       fclose(fp);
     }
     else
@@ -36,17 +45,22 @@ int main(int argc, char *argv[])
   }
 }
 
-void DoMore(FILE* fp)
+void DoMore(FILE* fp, int lnCount)
 {
   char line[LINELEN];
   int numLines = 0;
   int reply;
+  int totalLinesDisplayed = 0;
 
   while(fgets(line, LINELEN, fp))
   {
     if (numLines == PAGELEN)
     {
-      reply = SeeMore();
+      char *percent;
+      sprintf(percent, "%d%%", CalcPercent(totalLinesDisplayed, lnCount));
+      PrintStringReverseVideo(percent);
+
+      reply = SeeMore(fp);
       if (reply == 0)
       {
         break;
@@ -59,10 +73,11 @@ void DoMore(FILE* fp)
       exit(1);
     }
     numLines++;
+    totalLinesDisplayed++;
   }
 }
 
-int SeeMore()
+int SeeMore(FILE *fp)
 {
   int fd;
   char c;
@@ -104,4 +119,47 @@ int SetTcFlag(int flag, int onOff, FILE *fp)
 
   if (tcsetattr(fileno(fp), TCSAFLUSH, &newAttr) != 0)
     return -1;
+}
+
+void HandleSignals(int sigNum)
+{
+  if (sigNum == SIGINT)
+  {
+    PrintStringReverseVideo("Reset all flags\n");
+    exit(1);
+  }
+  else
+  {
+    printf("Nice try :)");
+  }
+}
+
+int GetFileLineCount(char *fileName)
+{
+  int count = 0;
+  FILE *fp;
+  char line[LINELEN];
+
+  fp = fopen(fileName, "r");
+  if (fp != NULL)
+  {
+    while(fgets(line, LINELEN, fp))
+    {
+      ++count;
+    }
+    fclose(fp);
+  }
+  return count;
+}
+
+void PrintStringReverseVideo(char *toPrint)
+{
+  printf("\033[7m");
+  printf("%s", toPrint);
+  printf("\033[m");
+}
+
+int CalcPercent(int numerator, int denominator)
+{
+  return (int)((float)numerator/(float)denominator * 100);
 }
